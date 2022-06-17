e4e88dbc56465fa06c3f7cb8624346f965a43348
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-06-17 11:00:49 -0400

    Add files via upload

--- commands/eval.js
+++ commands/eval.js
@@ -3,10 +3,13 @@ const Discord = require('discord.js')
 const config = require("../json/config.json");
 
 
+const figlet = require('figlet');
+
+
 module.exports = 
 {
     name: 'eval',
-    description: 'Run js code from a discord message',
+    description: 'Execute javascript code [DEVELOPER ONLY]',
     execute(message, args, client) 
     {
         try
@@ -14,13 +17,54 @@ module.exports =
             if (message.author.id == config.owner)
             {
                 
-                var result = message.content.split(" ").slice(1).join(" ")
-                let evaled = eval(result);
-    
+                var msg = message.content.split(" ").slice(1).join(" ")
+
+                let evaled = eval(msg);
+
+                if (evaled === undefined)
+                {
+                    if (msg.includes("console.log"))
+                    {
+                        var msgtmp = msg.split('"');
+
+                        var embed = new Discord.MessageEmbed()
+                        .addFields
+                        (
+                            { name: "Code", value: msg},
+                            { name: "Result", value: msgtmp[1]},
+                        )
+                        .setColor('BLACK');
+                        message.channel.send(embed);
+
+                        console.clear();
+                        figlet('Cat Bot', function(err, data) 
+                        {
+ 
+                          console.log(data);
+                        })
+                        return;
+                    }
+
+                    if (msg.includes("function"))
+                    {
+                        var embed = new Discord.MessageEmbed()
+                        .addFields
+                        (
+                            { name: "Code", value: msg},
+                            { name: "Message", value: "Cannot evaluate functions"},
+                        )
+                        .setColor('BLACK');
+                        message.channel.send(embed);
+                        return
+                    }
+                }
                 var embed = new Discord.MessageEmbed()
                 .setTitle("Cat Bot Evalatuion")
-                .setFooter(result)
-                .setDescription("Javascript executed")
+                .addFields
+                (
+                    { name: "Code", value: msg},
+                    { name: "Result", value: evaled},
+                )
                 .setColor('BLACK');
                 message.channel.send(embed);
             }
