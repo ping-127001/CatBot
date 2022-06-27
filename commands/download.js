20fb5c79bfc2683d0b994c504ecfef837d1a6fa9
romance <66000596+romance999@users.noreply.github.com>
2022-06-27 17:31:03 -0400

    Update to use nyx api

--- commands/download.js
+++ commands/download.js
@@ -1,25 +1,65 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+
+var url = `http://127.0.0.1:8080/API/download`;
+
+const request = require('request');
+
+const isReachable  = require('is-reachable');
+
+let options = {json: true};
 
 const config = require("../json/config.json");
 
 module.exports =
  {
     name: 'download',
-    description: 'Downlaod Nyx',
+    description: 'Get links to download Nyx',
     execute(message, args, client) 
     {
         try
         {
-            var embed = new Discord.MessageEmbed()
-            .addFields
-            (
-                { name: "Download Nyx", value: "N/A"},
-                { name: "Downlaod Nyx source", value: "https://github.com/ping-127001/Nyx/archive/refs/heads/main.zip"},
-                { name: "Downlaod Nyx Manager", value: "N/A"},
-                { name: "Downlaod Nyx Manager source", value: "https://github.com/ping-127001/NyxManager/archive/refs/heads/main.zip"},
-            )
-            .setColor('BLACK');
-            message.channel.send(embed);
+            message.channel.send("https://http.cat/102");
+            (async () => 
+            {
+                var reachable = await isReachable(url)
+                if (reachable)
+                {
+                    request(url, options, (res, json) => 
+                    {
+                        if (json.statusCode == 200)
+                        {
+                            message.channel.send("https://http.cat/200");
+
+                            var embed = new Discord.MessageEmbed()
+                            .addFields
+                            (
+                                { name: "API Status Code", value: '200'},
+                                { name: "Nyx Download", value: json.body.nyx},
+                                { name: "Nyx Source", value: json.body.nyxsrc},
+                                { name: "Nyx Manager", value: json.body.nyxmanager},
+                                { name: "Nyx Manager Source", value: json.body.nyxmanagersrc},
+                                { name: "Nyx Builder", value: json.body.nyxbuilder},
+                                { name: "Nyx Builder Source", value: json.body.nyxsbuildersrc},
+                            )
+                                .setFooter(`Nyx download links recieved from ${url}`)
+                                .setColor('BLACK');
+                                message.channel.send(embed);
+                        }
+                    })
+                }
+
+                if (!reachable)
+                {
+                    message.channel.send("https://http.cat/521");
+                    var embed = new Discord.MessageEmbed()
+                    .addFields
+                    (
+                    { name: "Message", value: url + ' is unreachable/or unresponsive'},
+                    )
+                    .setColor('BLACK');
+                    message.channel.send(embed); 
+                }
+            })();
         }
         catch (ex)
         {
